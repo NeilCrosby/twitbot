d5f432ec57f39e77087f7cec4f1e4dade87047f9
Neil Crosby <neil@thetenwordreview.com>
2008-11-08 22:22:59 +0000

    RssBot now only outputs to twitter at most once every 15 minutes, and if multiple posts have been made in that time it says how many occurred since last tweet, rather than posting them all

--- bot/RssTwitBot.php
+++ bot/RssTwitBot.php
@@ -27,6 +27,14 @@ class RssTwitBot extends BaseTwitBot {
         $password = $this->aOptions['password'];
         $bot = new Twitter( $username, $password );
 
+		// TODO: needs generalising out to the DB
+        $now = time();
+		if ( $now - $this->aOptions['last_data_time'] <= 60 * 15 ) { // 15 minute gaps
+			echo "<p>Not enough time passed - $now - ".$this->aOptions['last_data_time'].' - '.($now - $this->aOptions['last_data_time']).' - '.(60 * 15)."</p>";
+			return;
+		}
+
+		$itemsToPost = array();
         foreach( $feedItems as $item ) {
             $itemTime = strtotime($item->pubDate);
             
@@ -36,8 +44,14 @@ class RssTwitBot extends BaseTwitBot {
 
             $this->aOptions['last_data_time'] = $itemTime;
 
-            echo "<p>{$item->title}: {$item->link}</p>";
-            
+            //echo "<p>{$item->title}: {$item->link}</p>";
+            array_push($itemsToPost, $item);
+        }
+
+		$toPost = null;
+		if ( 1 == sizeof($itemsToPost) ) {
+			$item = $itemsToPost[0];
+			
             $titleLength = mb_strlen($item->title);
             $urlLength = mb_strlen('http://tinyurl.com/6dvl5n'); // a short url created by twitter - TODO switch to bit.ly (it's shorter)
             $shortDescAllowedLength = 140 - $urlLength - $titleLength - 3;
@@ -47,10 +61,17 @@ class RssTwitBot extends BaseTwitBot {
             if ( mb_strlen($shortDesc) > $shortDescAllowedLength ) {
                 $shortDesc = mb_substr($shortDesc, 0, $shortDescAllowedLength - 3).'...';
             }
-            
-            $bot->status->update( "{$item->title}: $shortDesc {$item->link}" );
-        }
 
+            $toPost = "{$item->title}: $shortDesc {$item->link}";
+		} else if ( 1 < sizeof($itemsToPost) ) {
+			$num = sizeof($itemsToPost);
+			$link = $feed->channel->link;
+            $toPost = "$num new items posted to $link";
+		}
+
+        if ($toPost) {
+			$bot->status->update($toPost);
+		}
     }
     
 }
